3f6e720bfedcc049c73e8a2fd14b8c06ec7edf78
George Offley <geo.offley@gmail.com>
2022-04-18 17:09:23 -0400

    Update MainContent.js

--- context-in-react/context-in-react/src/Components/MainContent.js
+++ context-in-react/context-in-react/src/Components/MainContent.js
@@ -1,10 +1,8 @@
 import React from "react"
 
-import { UserContext } from "../App";
 import UserBox from "./UserBox"
 
 const MainContent = () => {
-    const userNameContext = React.useContext(UserContext);
     return (
         <div className="bg-green-400 border-2">
             <h1>MainContent here!</h1>
